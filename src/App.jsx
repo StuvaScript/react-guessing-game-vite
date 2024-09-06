612824e60a6ba14b1c7ceca9564eea240fd954ab
Stu <stumac1000@yahoo.com>
2024-09-06 09:53:08 -0700

    Convert code from legacy code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 import NumberGuessingGame from "./NumberGuessingGame";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <NumberGuessingGame />
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <NumberGuessingGame />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+const App = () => {
+  return (
+    <div className="App">
+      <NumberGuessingGame />
+    </div>
+  );
+};
 
 export default App;
