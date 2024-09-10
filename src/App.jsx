267021185bf6948f45db0a2e94c4804f28e3dcd7
Stu <stumac1000@yahoo.com>
2024-09-10 12:58:07 -0700

    Delete comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,6 @@
 import NumberGuessingGame from "./NumberGuessingGame";
 import "./App.css";
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <NumberGuessingGame />
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
 const App = () => {
   return (
     <div className="App">
